1dbcc1aec39795b37dc817e2c0f3bf859287a3e6
ozgurerdogan1 <erdgn@gmail.com>
2023-05-21 22:14:12 +0300

    first init

---
+++ lib/board.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold();
+  }
+}
